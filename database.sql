77826f87bd03af9ee4e9cf9fb6f155f1ff31cee7
정지아 <58299651+cheocho@users.noreply.github.com>
2021-06-22 13:34:01 +0900

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,15 +3,8 @@ CREATE SCHEMA animations;
 USE animations;
 
 CREATE TABLE `animation` (
-	`name` VARCHAR(50) NOT NULL PRIMARY KEY DEFAULT '0',
+	`name` VARCHAR(70) NULL DEFAULT NULL,
 	`description` VARCHAR(50) NULL DEFAULT NULL
 );
 
-CREATE TABLE `animation_version` (
-	`username` CHAR(10) NOT NULL PRIMARY KEY DEFAULT '0',
-	`description_version` CHAR(10) NOT NULL,
-) 
 COLLATE='utf8_general_ci';
-
-INSERT INTO `animation_version` VALUES ('animation', '2020.06.03-0.08.7')
-INSERT INTO `animation_version` VALUES ('api', '2020.06.03-0v1')
